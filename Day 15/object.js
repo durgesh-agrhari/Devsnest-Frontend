1a80432979b6bfa3a731ed770183250ddf448d7a
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-07-12 15:11:31 +0530

    Create object.js

---
+++ Day 15/object.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p id="demo"></p>
+
+<script>
+// Create and display a variable:
+let person = "John Doe";
+document.getElementById("demo").innerHTML = person;
+</script>
+
+</body>
+</html>
