1341af56172f183df5db3368ce5094d50272205b
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-08-15 22:10:55 +0530

    Delete Nain 4.js

--- Day 9/Nain 4.js
+++
@@ -1,9 +0,0 @@
-function sumOfNumbers() {
-    var total = 0;
-    for(var i = 0; i < arguments.length; i++){
-        total += arguments[i];
-    }
-    return total;
-}
-var sum = sumOfNumbers(1,2,3);
-console.log(sum);
