f3b210f9cc7c4edc3521a7cfdbd8a9e76ea6c0c4
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-08-18 19:42:00 +0530

    Create App.js

---
+++ Day 19/App.js
@@ -0,0 +1,20 @@
+import Count from './components/Count'
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="cont">
+        <h1>COUNTER</h1>
+        </div>
+        <Count />
+        <Count/>
+        <Count />
+        <Count/>
+      </div>
+    
+  );
+}
+
+export default App;
